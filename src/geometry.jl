7f93f38cd2bcdccdffa6e0cfc0344a268459dc8c
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-03-29 14:51:49 +0200

    clean up SVector operations

--- src/geometry.jl
+++ src/geometry.jl
@@ -19,29 +19,29 @@ import WaterLily: ×
 function locate(x::SVector{T},tri::SMatrix{T}) where T
     # unpack the triangle vertices
     a,b,c = tri[:,1],tri[:,2],tri[:,3]
-    ab,ac,ap = b.-a,c.-a,x.-a
-    d1,d2 = sum(ab.*ap),sum(ac.*ap)
+    ab,ac,ap = b-a, c-a, x-a
+    d1,d2 = ab'ap, ac'ap
     # is point `a` closest?
     ((d1 ≤ 0) && (d2 ≤ 0)) && (return a)
     # is point `b` closest?
-    bp = x.-b
-    d3,d4 = sum(ab.*bp),sum(ac.*bp)
+    bp = x-b
+    d3,d4 = ab'bp, ac'bp
     ((d3 ≥ 0) && (d4 ≤ d3)) && (return b)
     # is point `c` closest?
-    cp = x.-c
-    d5,d6 = sum(ab.*cp),sum(ac.*cp)
+    cp = x-c
+    d5,d6 = ab'cp, ac'cp
     ((d6 ≥ 0) && (d5 ≤ d6)) && (return c)
     # is segment 'ab' closest?
     vc = d1*d4 - d3*d2
-    ((vc ≤ 0) && (d1 ≥ 0) && (d3 ≤ 0)) && (return a .+ ab.*d1 ./ (d1 - d3))
+    ((vc ≤ 0) && (d1 ≥ 0) && (d3 ≤ 0)) && (return a + (d1/(d1-d3))*ab)
     #  is segment 'ac' closest?
     vb = d5*d2 - d1*d6
-    ((vb ≤ 0) && (d2 ≥ 0) && (d6 ≤ 0)) && (return a .+ ac.*d2 ./ (d2 - d6))
+    ((vb ≤ 0) && (d2 ≥ 0) && (d6 ≤ 0)) && (return a + (d2/(d2-d6))*ac)
     # is segment 'bc' closest?
     va = d3*d6 - d5*d4
-    ((va ≤ 0) && (d4 ≥ d3) && (d5 ≥ d6)) && (return b .+ (c .- b) .* (d4 - d3) ./ ((d4 - d3) + (d5 - d6)))
+    ((va ≤ 0) && (d4 ≥ d3) && (d5 ≥ d6)) && (return b + ((d4-d3)/(d4-d3+d5-d6))*(c-b))
     # closest is interior to `abc`
     denom = one(T) / (va + vb + vc)
     v,w= vb*denom,vc*denom
-    return a .+ ab .* v .+ ac .* w
+    return a + v*ab +w*ac
 end

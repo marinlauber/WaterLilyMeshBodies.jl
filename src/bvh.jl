c846016767dcdc4a91c0a0a23691aff92942b349
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2026-04-06 10:03:58 +0200

    optimized closest and dist
    35% speed up on the benchmark.

--- src/bvh.jl
+++ src/bvh.jl
@@ -9,13 +9,15 @@ import ImplicitBVH: BoundingVolume, BBox, BSphere, memory_index, unsafe_isvirtua
 @fastmath @inline inside(x::SVector, b::BSphere) = √sum(abs2,x .- b.x) - b.r ≤ 4
 
 # compute the square distance to primitive
-dist(x, b::BSphere) = max(√sum(abs2,x .- b.x) - b.r, 0)^2
-function dist(x, b::BBox)
-    c = (b.up .+ b.lo) ./ 2
-    r = (b.up .- b.lo) ./ 2
-    sum(abs2, max.(abs.(x .- c) .- r, 0))
+@fastmath @inline dist(x, b::BSphere) = max(√sum(abs2,x .- b.x) - b.r, 0)^2
+@fastmath @inline function dist(x, b::BBox)
+    s = zero(eltype(x))
+    @inbounds for i in eachindex(b.up,b.lo)
+        d = abs(x[i] - 0.5f0*(b.up[i] + b.lo[i])) - 0.5f0*(b.up[i] - b.lo[i])
+        d>0 && (s+=d^2)
+    end; s
 end
-dist(x, b::BoundingVolume) = dist(x, b.volume)
+@inline dist(x, b::BoundingVolume) = dist(x, b.volume)
 
 # traverse the BVH
 @inline function closest(x::SVector{D,T},bvh::ImplicitBVH.BVH,mesh;init_d²=floatmax(T),verbose=false) where {D,T}
@@ -27,15 +29,22 @@ dist(x, b::BoundingVolume) = dist(x, b.volume)
         @inbounds j = memory_index(tree,i)
         if j ≤ length_nodes # we are on a node
             verbose && (ncheck += 1)
-            dist(x, bvh.nodes[j]) < best.d² && (i = 2i; continue) # go deeper if closer than current best
+            dist(x, @inbounds bvh.nodes[j]) < best.d² && (i = 2i; continue) # go deeper if closer than current best
         else # we reached a leaf
             verbose && (lcheck += 1)
-            if dist(x, bvh.leaves[j-length_nodes]) ≤ best.d²
+            if dist(x, @inbounds bvh.leaves[j-length_nodes]) ≤ best.d²
                 verbose && (tcheck += 1)
                 @inbounds j = bvh.leaves[j-length_nodes].index # correct index in mesh
-                n,p = normal(mesh[j]),locate(x,mesh[j]) # expensive...
+                tri = @inbounds mesh[j]
+                p = locate(x,tri)
                 d² = sum(abs2,x-p)
-                (best.index == 0 || d²<best.d² || (d²≈best.d² && abs((x-p)'n)>abs((x-best.p)'best.n))) && (best = (;index=Int32(j),d²,n,p))  # Replace current best
+                if best.index == 0 || d² < best.d²
+                    n = normal(tri)
+                    best = (;index=Int32(j),d²,n,p)
+                elseif d² ≈ best.d²
+                    n = normal(tri)
+                    abs((x-p)'n) > abs((x-best.p)'best.n) && (best = (;index=Int32(j),d²,n,p))
+                end
             end
         end
         i = i>>trailing_ones(i)+1 # go to sibling, or uncle etc.
